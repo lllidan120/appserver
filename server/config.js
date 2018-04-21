78393e69127a53d3b409c58d44699d47ad2597e2
lllidan120 <lllidan120@163.com>
2018-04-21 15:21:23 +0800

    'first'

---
+++ server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	db:'mongodb://localhost:27017/app',
+	defaultPort: 3001,
+	staticVirtualPath: '/static',
+	staticRealPath: '/server/yjapp/opensource/',
+
+	zipReturn: 'diff' // full 全量
+}
